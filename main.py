dc8747153a8e7e2352e8986dfe5c675cd4bb0ed1
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-07-30 11:07:17 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import bs4
+import requests
+
+
+def main():
+    """Main function"""
+    print('This is main')
+    print('\n***************\n')
+    print(dir(bs4))
+    print('\n***************\n')
+    print(dir(requests))
+
+
+if __name__=='__main__':
+    main()
