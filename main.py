7b426d0792bbd035d43199cffc3e1caa3bd5ee7e
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-08-21 22:31:33 +0700

    Steps 1-2 completed

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def download_book(url, storage_folder, book_filename):
 
     book_fullpath = f'{storage_folder}/{book_filename}'
     print(f'Save to file - {book_fullpath}')
+
     with open(book_fullpath, 'wb') as file:
         file.write(response.content)
     return response.status_code
@@ -31,15 +32,16 @@ def main():
     if not os.path.exists(storage_folder):
         os.makedirs(storage_folder)
 
-    for i in range(11):
-        url = f'https://tululu.org/txt.php?id={i}'
-        book_filename = f'id{i}.txt'
-        try:
-            response = download_book(url=url, \
-                                     storage_folder=storage_folder, \
-                                     book_filename=book_filename)
-        except requests.exceptions.HTTPError as err:
-            print('Main page redirect')
+    book_id = 32168     # книга Артур Кларк - Пески Марса
+    url = f'https://tululu.org/txt.php?id={book_id}'
+    book_filename = f'id{book_id}.txt'
+
+    try:
+        response = download_book(url=url, \
+                                    storage_folder=storage_folder, \
+                                    book_filename=book_filename)
+    except requests.exceptions.HTTPError as err:
+        print('Main page redirect')
 
 
 if __name__=='__main__':
