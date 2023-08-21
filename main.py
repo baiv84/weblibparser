73ef40d719bbb41b814325f6517d9b942906f9b8
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-08-21 23:31:44 +0700

    Step 3 - download books from tululu.org

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_book(url, storage_folder, book_filename):
+def download_book_by_url(url, \
+                  book_filename, \
+                  storage_folder='books'):
     '''Download file as a text'''
+    if not os.path.exists(storage_folder):
+        os.makedirs(storage_folder)
+
     response = requests.get(url)
     response.raise_for_status() 
     check_for_redirect(response)
@@ -23,25 +28,48 @@ def download_book(url, storage_folder, book_filename):
     return response.status_code
 
 
+def download_bunch_of_books(id_start=1, \
+                            id_stop=100, \
+                            storage_folder='books'):
+    '''Download books with id in range [id_start, id_stop]'''
+
+    if not os.path.exists(storage_folder):
+        os.makedirs(storage_folder)
+
+    for id in range(id_start, id_stop+1):
+        url = f'https://tululu.org/txt.php?id={id}'
+        book_filename = f'id{id}.txt'
+
+        try:
+            response = download_book_by_url(url=url, \
+                                            storage_folder=storage_folder, \
+                                            book_filename=book_filename)
+        except requests.exceptions.HTTPError as err:
+            print(f'Main page redirect - {book_filename}')
+
+
 def main():
     '''Program entry point'''
     env = Env()
     env.read_env()
     storage_folder = env('STORAGE_FOLDER')
 
-    if not os.path.exists(storage_folder):
-        os.makedirs(storage_folder)
-
-    book_id = 32168     # книга Артур Кларк - Пески Марса
+    #-----Download Arthur Clark book-----
+    print('Download book - Arthur Clark The sands of Mars')
+    book_id = 32168
     url = f'https://tululu.org/txt.php?id={book_id}'
     book_filename = f'id{book_id}.txt'
 
     try:
-        response = download_book(url=url, \
-                                    storage_folder=storage_folder, \
-                                    book_filename=book_filename)
+        response = download_book_by_url(url=url, \
+                                        storage_folder=storage_folder, \
+                                        book_filename=book_filename)
     except requests.exceptions.HTTPError as err:
-        print('Main page redirect')
+        print(f'Main page redirect - {book_filename}')
+
+
+    #-----Download set of books-----
+    download_bunch_of_books(id_start=1, id_stop=10, storage_folder=storage_folder)
 
 
 if __name__=='__main__':
