880d9ab6a74849fe7fd7a4c97929b2386cd7ba92
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-08-21 21:53:15 +0700

    Initial commit to develop - fix styles

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-import bs4
+import os
 import requests
+from environs import Env
+
+
+def check_for_redirect(response):
+    '''Check for redirect URL'''
+    if response.url == 'https://tululu.org/':
+        raise requests.HTTPError
+
+
+def download_book(url, storage_folder, book_filename):
+    '''Download file as a text'''
+    response = requests.get(url)
+    response.raise_for_status() 
+    check_for_redirect(response)
+
+    book_fullpath = f'{storage_folder}/{book_filename}'
+    print(f'Save to file - {book_fullpath}')
+    with open(book_fullpath, 'wb') as file:
+        file.write(response.content)
+    return response.status_code
 
 
 def main():
-    """Main function"""
-    print('This is main')
-    print('\n***************\n')
-    print(dir(bs4))
-    print('\n***************\n')
-    print(dir(requests))
+    '''Program entry point'''
+    env = Env()
+    env.read_env()
+    storage_folder = env('STORAGE_FOLDER')
+
+    if not os.path.exists(storage_folder):
+        os.makedirs(storage_folder)
+
+    for i in range(11):
+        url = f'https://tululu.org/txt.php?id={i}'
+        book_filename = f'id{i}.txt'
+        try:
+            response = download_book(url=url, \
+                                     storage_folder=storage_folder, \
+                                     book_filename=book_filename)
+        except requests.exceptions.HTTPError as err:
+            print('Main page redirect')
 
 
 if __name__=='__main__':
