2ab64d8a867a9cc4f9d997548790a14d7d296523
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-08-24 23:23:53 +0700

    Step 6 - parse book name and author

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from environs import Env
+from bs4 import BeautifulSoup
 
 
 def check_for_redirect(response):
@@ -32,7 +33,6 @@ def download_bunch_of_books(id_start=1,
                             id_stop=100,
                             storage_folder='books'):
     '''Download books with id in range [id_start, id_stop]'''
-
     if not os.path.exists(storage_folder):
         os.makedirs(storage_folder)
 
@@ -48,29 +48,38 @@ def download_bunch_of_books(id_start=1,
             print(f'Main page redirect - {book_filename}')
 
 
+def extract_book_name_author(id):
+    '''Extract book name and author'''
+    url = f'https://tululu.org/b{id}/'
+
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    book = soup.find('div', id='content').find('h1').text
+    book_name, book_author = book.split('::')
+
+    return {
+        'id': id,
+        'name': book_name.lstrip().rstrip(),
+        'author': book_author.lstrip().rstrip()
+    }
+
+
 def main():
     '''Program entry point'''
     env = Env()
     env.read_env()
     storage_folder = env('STORAGE_FOLDER')
 
-    # -----Download Arthur Clark book-----
-    print('Download book - Arthur Clark The sands of Mars')
-    book_id = 32168
-    url = f'https://tululu.org/txt.php?id={book_id}'
-    book_filename = f'id{book_id}.txt'
-
-    try:
-        response = download_book_by_url(url=url,
-                                        storage_folder=storage_folder,
-                                        book_filename=book_filename)
-    except requests.exceptions.HTTPError as err:
-        print(f'Main page redirect - {book_filename}')
-
-    # ----Download set of books-----
-    download_bunch_of_books(id_start=1,
-                            id_stop=10,
-                            storage_folder=storage_folder)
+    for book_id in range(1000):
+        try:
+            book = extract_book_name_author(id=book_id)
+            print(f'id - {book["id"]}, книга - {book["name"]},',
+                  f'автор - {book["author"]}')
+        except requests.exceptions.HTTPError as err:
+            pass
 
 
 if __name__ == '__main__':
