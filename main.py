a32937d0169b0cb5c131ea0a72e09678c178c633
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-08-27 00:58:41 +0700

    Step 10 - download book covers

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from environs import Env
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse, unquote
 
 
 def check_for_redirect(response):
@@ -12,48 +12,10 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_book_by_url(url,
-                         book_filename,
-                         storage_folder='books'):
-    '''Download file as a text'''
-    if not os.path.exists(storage_folder):
-        os.makedirs(storage_folder)
-
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
-
-    book_fullpath = f'{storage_folder}/{book_filename}'
-    print(f'Save to file - {book_fullpath}')
-
-    with open(book_fullpath, 'wb') as file:
-        file.write(response.content)
-    return response.status_code
-
-
-def download_bunch_of_books(id_start=1,
-                            id_stop=100,
-                            storage_folder='books'):
-    '''Download books with id in range [id_start, id_stop]'''
-    if not os.path.exists(storage_folder):
-        os.makedirs(storage_folder)
-
-    for id in range(id_start, id_stop+1):
-        url = f'https://tululu.org/txt.php?id={id}'
-        book_filename = f'id{id}.txt'
-
-        try:
-            response = download_book_by_url(url=url,
-                                            storage_folder=storage_folder,
-                                            book_filename=book_filename)
-        except requests.exceptions.HTTPError as err:
-            print(f'Main page redirect - {book_filename}')
-
-
-def extract_book_name_author(id):
-    '''Extract book name and author'''
+def serialize_book(book_id):
+    '''Serialize book with particular book_id'''
     base_url = 'https://tululu.org'
-    book_url = urljoin(base_url, f'b{id}')
+    book_url = urljoin(base_url, f'b{book_id}')
 
     response = requests.get(book_url)
     response.raise_for_status()
@@ -71,8 +33,8 @@ def extract_book_name_author(id):
     image_url = urljoin(base_url, image_url)
 
     return {
-        'id': id,
-        'url': f'http://tululu.org/txt.php?id={id}',
+        'id': book_id,
+        'url': f'http://tululu.org/txt.php?id={book_id}',
         'image_url': image_url,
         'name': f'{book_name}.txt',
         'author': book_author,
@@ -98,27 +60,47 @@ def download_txt(url, filename, folder='books/'):
     return book_fullpath
 
 
-def download_image(url, folder='images/'):
-    '''download image to the folder'''
+def download_image(url, filename, folder='images/'):
+    '''Download image to the folder'''
+    folder = sanitize_filename(folder)
+    filename = sanitize_filename(filename)
+    image_fullpath = os.path.join(folder, filename)
+
     if not os.path.exists(folder):
         os.makedirs(folder)
+
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    with open(image_fullpath, 'wb') as file:
+        file.write(response.content)
+
+    return image_fullpath
 
 
 def main():
     '''Program entry point'''
     env = Env()
     env.read_env()
-    storage_folder = env('STORAGE_FOLDER')
+    book_folder = env('BOOK_FOLDER')
+    image_folder = env('IMAGE_FOLDER')
 
-    for book_id in range(10):
+    for book_id in range(100):
         try:
-            book = extract_book_name_author(book_id)
+            book = serialize_book(book_id)
             book_url = book['url']
             image_url = book['image_url']
             book_name = f"{book['id']}. {book['name']}"
-            filepath = download_txt(book_url, book_name, folder=storage_folder)
-            print(filepath)
-            print(image_url)
+            image_name = unquote(urlparse(image_url).path.split("/")[-1])
+
+            image_filepath = download_image(image_url, image_name,
+                                            folder=image_folder)
+            book_filepath = download_txt(book_url, book_name,
+                                         folder=book_folder)
+
+            print(image_filepath)
+            print(book_filepath)
         except requests.exceptions.HTTPError as err:
             pass
 
