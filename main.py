1006af2afbf9d9e28c62098cb73b6ad61893539e
baiv84 <65267292+baiv84@users.noreply.github.com>
2023-08-27 16:09:42 +0700

    Step 13 - refactor book parsing function

--- main.py
+++ main.py
@@ -12,43 +12,6 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def serialize_book(book_id):
-    '''Serialize book with particular book_id'''
-    base_url = 'https://tululu.org'
-    book_url = urljoin(base_url, f'b{book_id}')
-
-    response = requests.get(book_url)
-    response.raise_for_status()
-    check_for_redirect(response)
-
-    soup = BeautifulSoup(response.text, 'lxml')
-    book = soup.find('div', id='content').find('h1').text
-    book_name, book_author = book.split('::')
-
-    book_name = book_name.lstrip().rstrip()
-    book_author = book_author.lstrip().rstrip()
-
-    image_url = soup.find('div', class_="bookimage") \
-                    .find('a').find('img')['src']
-    image_url = urljoin(base_url, image_url)
-
-    comments = soup.find_all('div', class_='texts')
-    comments_txt = [comment.find('span').text for comment in comments]
-
-    genres = soup.find('span', class_='d_book').find_all('a')
-    genres = [genre.text for genre in genres]
-
-    return {
-        'id': book_id,
-        'url': f'http://tululu.org/txt.php?id={book_id}',
-        'image_url': image_url,
-        'filename': f'{book_name}.txt',
-        'author': book_author,
-        'comments': '\n'.join(comments_txt),
-        'genres': genres,
-    }
-
-
 def download_txt(url, filename, folder='books/'):
     '''Download file as a text'''
     folder = sanitize_filename(folder)
@@ -87,32 +50,70 @@ def download_image(url, filename, folder='images/'):
     return image_fullpath
 
 
+def parse_book_page(html):
+    '''Parse book page for fields'''
+    soup = BeautifulSoup(html, 'lxml')
+    book = soup.find('div', id='content').find('h1').text
+    book_name, book_author = book.split('::')
+
+    book_name = book_name.lstrip().rstrip()
+    book_author = book_author.lstrip().rstrip()
+
+    image_url = soup.find('div', class_="bookimage") \
+                    .find('a').find('img')['src']
+
+    comments = soup.find_all('div', class_='texts')
+    comments_txt = [comment.find('span').text for comment in comments]
+
+    genres = soup.find('span', class_='d_book').find_all('a')
+    genres = [genre.text for genre in genres]
+
+    return {
+        'book_name': book_name,
+        'book_author': book_author,
+        'book_comments': '\n'.join(comments_txt),
+        'book_genres': genres,
+        'book_filename': f'{book_name}.txt',
+        'book_image_url': image_url,
+    }
+
+
 def main():
     '''Program entry point'''
     env = Env()
     env.read_env()
     book_folder = env('BOOK_FOLDER')
     image_folder = env('IMAGE_FOLDER')
+    base_url = env('BASE_URL')
 
-    for book_id in range(10):
+    start_id = 1
+    end_id = 50
+
+    for book_id in range(start_id, end_id+1):
         try:
-            book = serialize_book(book_id)
-            book_url = book['url']
-            image_url = book['image_url']
-            book_comments = book['comments']
-            book_genres = book['genres']
-            book_name = f"{book['id']}. {book['filename']}"
-            image_name = unquote(urlparse(image_url).path.split("/")[-1])
+            book_url = urljoin(base_url, f'b{book_id}')
+            response = requests.get(book_url)
+            response.raise_for_status()
+            check_for_redirect(response)
 
-            image_filepath = download_image(image_url, image_name,
+            book = parse_book_page(response.text)
+            book_txt_url = f'http://tululu.org/txt.php?id={book_id}'
+
+            book_image_url = book['book_image_url']
+            book_image_url = urljoin(base_url, book_image_url)
+
+            book_comments = book['book_comments']
+            book_genres = book['book_genres']
+            book_name = f"{book_id}. {book['book_filename']}"
+
+            image_filename = unquote(urlparse(book_image_url).path.split("/")[-1])
+            image_filepath = download_image(book_image_url, image_filename,
                                             folder=image_folder)
-            book_filepath = download_txt(book_url, book_name,
+            book_filepath = download_txt(book_txt_url, book_name,
                                          folder=book_folder)
 
             print('\n****************')
-            print(image_filepath)
             print(book_filepath)
-            print(book_comments)
             print(book_genres)
         except requests.exceptions.HTTPError as err:
             pass
